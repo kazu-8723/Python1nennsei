bcd05b271ec9fb0bf0b5e0391df6692e7478e533
Matsuta Kazuya <mttkzy.8723@gmail.com>
2025-09-06 16:35:15 +0900

    モジュール名の省略化

--- lesson16_import.py
+++ lesson16_import.py
@@ -1,6 +1,6 @@
-# lesson16_tax.pyをインポート
-import lesson16_tax
+# lesson16_tax.pyをインポート as taxとすることでモジュール名を省略名で書くことが可能
+import lesson16_tax as tax
 
-print(lesson16_tax.postTaxPrice(120), "円")
-print(lesson16_tax.postTaxPrice(128), "円")
-print(lesson16_tax.postTaxPrice(980), "円")
+print(tax.postTaxPrice(120), "円")
+print(tax.postTaxPrice(128), "円")
+# print(lesson16_tax.postTaxPrice(980), "円")
