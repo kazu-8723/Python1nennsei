1c1cb84d3e665541f1b9cf8cd6622633912a83f2
Matsuta Kazuya <mttkzy.8723@gmail.com>
2025-09-06 16:20:07 +0900

    引数or戻り値のみの関数の使い道

--- lesson15.py
+++ lesson15.py
@@ -2,6 +2,24 @@
 def sayhello():
 # 関数内の設定
     print("こんにちは")
-
 # 関数の内部を呼び出す
 sayhello()
+
+
+# 引数のみある関数の作成
+def sayhello2(name):
+# 関数内の設定
+    print("こんにちは、" + name + "さん。")
+# 関数の内部を呼び出す
+sayhello2("ピカチュウ")
+
+
+import random
+# 関数作成
+def omikuji():
+# リスト作成
+    kuji = ['大吉', '中吉', '小吉', '凶']
+# リスト内からランダムで選ばれたものを戻り値として返す
+    return random.choice(kuji)
+kekka = omikuji()
+print("結果は", kekka, "です。")
