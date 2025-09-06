f99c95fa72402283b5ffd5df5abb9606b80b2870
Matsuta Kazuya <mttkzy.8723@gmail.com>
2025-09-06 15:44:33 +0900

    elifの追加

--- lesson13.py
+++ lesson13.py
@@ -1,8 +1,11 @@
-score = 60
+score = 80
 # 80点以上の場合の処理
 if score >= 80:
     print("やったね")
     print("その調子")
-# 80点未満の場合の処理
+# 70点以上の場合の処理
+elif score >= 70:
+    print("ギリギリ")
+# 70点未満の場合の処理
 else:
     print("残念")
