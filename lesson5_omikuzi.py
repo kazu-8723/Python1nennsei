3a9ac5dbe8b0364358ce56facb03e0f5ee5cb085
Matsuta Kazuya <mttkzy.8723@gmail.com>
2025-09-06 15:28:09 +0900

    結果の追加

--- lesson5_omikuzi.py
+++ lesson5_omikuzi.py
@@ -1,6 +1,6 @@
 # randomをimport
 import random
 # 関数omikuziを作成
-omikuzi = ["大吉", "中吉", "小吉", "凶"]
+omikuzi = ["大吉", "中吉", "小吉", "凶", "半吉", "末吉", "大凶", "大大吉"]
 # 関数omikuziをランダムで選んで表示
 print(random.choice(omikuzi))
